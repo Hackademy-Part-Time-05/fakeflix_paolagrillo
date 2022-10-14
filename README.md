569e53d693355ab4763a8f8c4a638ab6655aebb8
Francesco <fra.mansi@gmail.com>
2022-10-15 00:22:41 +0200

    Add existing file

--- README.md
+++ README.md
@@ -7,5 +7,4 @@ Ripetere tutte le operazioni viste durante il live Coding e in aggiunta:
 -   Inserire una nuova rotta nel menu chiamata: Serie TV
 -   Utilizzare le immagini presenti in: public/img/esercizio
 -   Aggiungere in Homepage un secondo pulsante che rimandi direttamente ai film
-# fakeflix
-# fakeflix
+    go
---
+++ resources/views/catalogue.blade.php
@@ -0,0 +1,61 @@
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Netflix</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="/css/style.css" />
+
+<body>
+    <div class="wrapper overflow-hidden">
+        <header>
+            <div class="netflixLogo">
+                <a id="logo">
+                    <img src="/img/logo.png" alt="Logo Image">
+                </a>
+            </div>
+            <nav class="main-nav">
+                <a href="#">Home</a>
+                <a href="#">Film</a>
+
+            </nav>
+        </header>
+
+        <section class="main-container ">
+            <div class="location" id="home">
+                <h1 id="home">Film su FakeFlix</h1>
+                <div class="box">
+                    <a href="">
+                        <img src="/img/cover/breaking-bad.jpg" alt="">
+                    </a>
+                    {{-- <a href="">
+                        <img src="/img/cover/the-boys.jpg" alt="">
+                    </a>
+
+                    <a href="">
+                        <img src="/img/cover/dahmer.jpg" alt="">
+                    </a>
+                    <a href="">
+                        <img src="/img/cover/friends.jpg" alt="">
+                    </a>
+                    <a href="">
+                        <img src="/img/cover/hof.jpg" alt="">
+                    </a>
+                    <a href="">
+                        <img src="/img/cover/the-office.jpg" alt="">
+                    </a> --}}
+
+                </div>
+            </div>
+
+        </section>
+
+        <footer>
+
+        </footer>
+    </div>
+</body>
+
+</html>
